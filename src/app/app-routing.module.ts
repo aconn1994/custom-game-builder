79325e451fd91702b259d8b5dd2fcaf2556d788a
Adam Connolly <adam.m.connolly94@gmail.com>
2021-04-05 13:38:02 -0400

    implement Lazy Loading to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,13 @@
-import { CounterStrikeGlobalOffensiveComponent } from './features/modules/games/counter-strike-global-offensive/counter-strike-global-offensive.component';
-import { LeagueOfLegendsComponent } from './features/modules/games/league-of-legends/league-of-legends.component';
-import { RainbowSixSiegeComponent } from './features/modules/games/rainbow-six-siege/rainbow-six-siege.component';
-import { CustomGameComponent } from './features/modules/games/custom-game/custom-game.component';
+import { HomeComponent } from './features/modules/base/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './features/modules/base/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'custom-game', component: CustomGameComponent },
-  { path: 'r6siege', component: RainbowSixSiegeComponent },
-  { path: 'lol', component: LeagueOfLegendsComponent },
-  { path: 'csgo', component: CounterStrikeGlobalOffensiveComponent }
+  {
+    path: 'game',
+    loadChildren: () => import('./features/modules/games/games-routing.module').then(m => m.GamesRoutingModule)
+  }
 ];
 
 @NgModule({
---
+++ src/app/features/modules/games/games-routing.module.ts
@@ -0,0 +1,30 @@
+import { NavbarComponent } from './../base/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CounterStrikeGlobalOffensiveComponent } from './counter-strike-global-offensive/counter-strike-global-offensive.component';
+import { CustomGameComponent } from './custom-game/custom-game.component';
+import { LeagueOfLegendsComponent } from './league-of-legends/league-of-legends.component';
+import { RainbowSixSiegeComponent } from './rainbow-six-siege/rainbow-six-siege.component';
+
+const routes = [
+    {
+        path: '',
+        children: [
+            { path: '', redirectTo: '', pathMatch: 'full'},
+            { path: 'custom-game', component: CustomGameComponent },
+            { path: 'r6siege', component: RainbowSixSiegeComponent },
+            { path: 'lol', component: LeagueOfLegendsComponent },
+            { path: 'csgo', component: CounterStrikeGlobalOffensiveComponent }
+        ]
+    }
+];
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes)
+    ]
+})
+export class GamesRoutingModule {}
